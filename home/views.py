8f7d7988c1ae52055053a1f42938fd5591de0a06
ubaid_patel <fakeuser595@gmail.com>
2022-11-22 22:02:05 +0530

    Added Signup Functionality

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -27,6 +28,20 @@ def login_user(request):
     return render(request,'login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        mobile = request.POST.get("mobile")
+        fname = request.POST.get("firstname")
+        lname = request.POST.get("lastname")
+        user_email = request.POST.get("email")
+        passw = request.POST.get("password")
+        user_create = User.objects.create_user(username=mobile,email=user_email,password=passw)
+        user_create.first_name = fname
+        user_create.last_name = lname
+        if str(user_create) == str(mobile):
+            login(request,user_create)
+            messages.success(request,"Signup Success you will be redirected")
+            context={"meta_redirect":"refresh"}
+            return render(request,'signup.html',context)
     return render(request,'signup.html')
 
 def myprofile(request):
